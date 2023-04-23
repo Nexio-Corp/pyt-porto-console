92773a90e52ab4552d717d4c45a4875208f75509
Caua Rojas <cauarojas@hotmail.com>
2023-04-23 19:34:32 -0300

    removi contador e adicionei comentarios em bikes

--- bike.py
+++ bike.py
@@ -8,27 +8,25 @@ def vistoria(user: dict[str, str]):
     print("Dados enviados! Para o seguinte passo, é necessário estar com a sua bike do seu lado.")
     print("Agora precisaremos de 6 fotos da sua bike:")
 
-    i = 0
-    soma = ""
-    while i < 6:
-        i += 1
+    fotos = ""
+    while len(fotos):
         escolhaFoto = input(
             "Para simular o envio de fotos escolha as opções abaixo:"
             "\n1 - Foto autêntica\n2 - Foto da internet(Falsa)"
             "\n3 - Foto de uma bike de outro modelo"
             "\n4 - Foto de uma bike com dano"
-            f"\n({7 - i}) fotos restantes.\n")
-        soma += escolhaFoto
-    if soma == "111111":
+            f"\n({6 - len(fotos)}) fotos restantes.\n")
+        fotos += escolhaFoto
+    if fotos == "111111":  # 111111 é o valor que representa que todas as fotos são autênticas
         print("Vistoria executada com sucesso! A apólice foi enviada para o seu email.")
-    elif "2" in soma:
+    elif "2" in fotos:  # 2 é o valor que representa que uma das fotos é falsa
         print(
             "Identificamos fotos falsas, por favor envie fotos reais.")
-    elif "3" in soma:
+    elif "3" in fotos:  # 3 é o valor que representa que uma das fotos é de uma bike de outro modelo
         print(
             "Identificamos fotos com uma bike de outro modelo,"
             " por favor envie fotos com a sua bike cadastrada.")
-    elif "4" in soma:
+    elif "4" in fotos:  # 4 é o valor que representa que uma das fotos é de uma bike com dano
         print(
             "Identificamos fotos com uma bike danificada, infelizmente o seguro desse bike.")
     else:
